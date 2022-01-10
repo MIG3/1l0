8012394d15e02ee9669b793f8859e242f2c13c9c
Максим Губин <$mgubin@fil-it.ru$>
2022-01-10 15:51:50 +0300

    Убраны комментарии в main()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -121,8 +121,8 @@ public class Main
 
     public static void main(String[] args)
     {
-//        ArrayOddNumbers();
-//        MaxToMinElements();
+        ArrayOddNumbers();
+        MaxToMinElements();
         Matrix();
     }
 }
